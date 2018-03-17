9dc821236c0fa0f4ed6ac38b411c1a7aba7718e2
Shiliu Wang <shiliu.wang@intel.com>
2018-03-17 13:26:57 +0800

    Define superior parameters more reasonably and welcome back ReLU
    By experiment, i find out that the learning doesn't converge all the time.
    Actually it fails a lot, it makes me think about the root cause of those failure.
    Intuitively, too big learning rate will make the weight falling too fast leading
    to lose too much neurons. Last commit, i introduce LeakyReLU to cover the issue.
    
    And now as I thought it through, I could mathimatically guarantee enough active
    neurons by the way to initialize weight and bias, and learning rate as well.
    The detail idea is written as comment in code, unlike BN, we only cover the initialize
    state. For simple problem like MNist, it's enough. Maybe in future for more complicate
    models, BN is actually needed.

--- bp.py
+++ bp.py
@@ -4,6 +4,7 @@ from read_mnist import readMnistData
 
 verbose = False
 
+
 class Layer:
   def __init__(self, name, size, prev, activation):
     self._name = name
@@ -60,6 +61,7 @@ class Layer:
     if self._prev != None:
       self._prev.backward(self._dLdInput)
 
+
 class InputLayer(Layer):
   def __init__(self, size):
     Layer.__init__(self, "input", size, None, None)
@@ -68,16 +70,18 @@ class InputLayer(Layer):
     self.setBatchSize(batchSize)
     np.divide(samples, 256.0, self._output)
     np.add(samples, 0, self._result)
+
   def backward(self, dLdOutput):
     pass
+
 
 class FCLayer(Layer):
   def __init__(self, name, size, prev, activation):
     Layer.__init__(self, name, size, prev, activation)
     assert prev != None
     # randomly initiate weight and bias
-    self._w = np.random.uniform(0, 2.0/prev._size, size=(size, prev._size))
-    self._b = np.random.normal(scale=1.0, size=(size, 1))
+    self._w = np.random.uniform(0, 2.0 / 3.0 /prev._size, size=(size, prev._size))
+    self._b = np.random.uniform(0, 0.5, size=(size, 1))
     self._dLdw = np.zeros((size, prev._size))
     self._dLdb = np.zeros((size, 1))
 
@@ -106,15 +110,19 @@ class FCLayer(Layer):
     np.add(self._w, self._dLdw * -rate, self._w)
     np.add(self._b, self._dLdb * -rate, self._b)
 
+
 def relu(result, output=None):
   return np.maximum(result, 0, output)
 
+
 def leaky_relu(result, alpha, output=None):
-  return np.maximum(result, result*alpha, output)
+  return np.maximum(result, result * alpha, output)
+
 
 def softmax(result, output=None):
   temp = np.exp(result - np.max(result, 0))
   return np.divide(temp, np.sum(temp, 0), output)
+
 
 class Activation:
   def __init__(self, size):
@@ -126,37 +134,43 @@ class Activation:
 
   def calc(self, result, output):
     pass
+
   def deri(self, dLdOutput, dLdResult):
     pass
+
 
 class ReLU(Activation):
   def calc(self, result, output):
     np.add(result, 0, self._input)
     relu(result, output)
+
   def deri(self, dLdOutput, dLdResult):
-    dLdResult[self._input<=0] = 0
-    dLdResult[self._input>0] = 1
+    dLdResult[self._input <= 0] = 0
+    dLdResult[self._input > 0] = 1
     np.multiply(dLdResult, dLdOutput, dLdResult)
+
 
 class LeakyReLU(Activation):
   def __init__(self, size, alpha):
     Activation.__init__(self, size)
     self._alpha = alpha
+
   def calc(self, result, output):
     np.add(result, 0, self._input)
     leaky_relu(result, self._alpha, output)
+
   def deri(self, dLdOutput, dLdResult):
-    dLdResult[self._input<=0] = self._alpha
-    dLdResult[self._input>0] = 1
+    dLdResult[self._input <= 0] = self._alpha
+    dLdResult[self._input > 0] = 1
     np.multiply(dLdResult, dLdOutput, dLdResult)
 
 
 if __name__ == "__main__":
   output = 10
-  input = 28*28
+  input = 28 * 28
   # setup network graph
   layers = [600, 1000, 300]
-  alpha = 0.1
+  alpha = 0
   IL = InputLayer(input)
   FC1 = FCLayer("fc1", layers[0], IL, LeakyReLU(layers[0], alpha))
   FC2 = FCLayer("fc2", layers[1], FC1, LeakyReLU(layers[1], alpha))
@@ -166,20 +180,36 @@ if __name__ == "__main__":
   train, test = readMnistData("train-images.idx3-ubyte", "train-labels.idx1-ubyte", 12)
   batchSize = 100
   epoch = 20
+  """
+  We want the initial expectation of all layers' result to be fixed.
+  And for using ReLU, we want it to be mostly positive, so we choose 0.5 over 0.
+  For input, we normalize it by dividing 256.(Actually, could be better normalized)
+  For each layer's result:
+   result[i]= ReLU(result[i-1]) * w[i] + b[i] ~= E(w[i]) * sum(ReLU(result[i-1])) + E(b[i])
+  Since ReLU drops out the negative input connections,
+  by casually estimating, sum(ReLU(X)) ~= sum(X) * 1.5
+   E(sum(ReLU)) > E(sum) ~= input_size * E(result[i-1]) * 1.5 = input_size * 0.75
+   E(W) * input_size * 0.75 + E(b) = 0.5
+  So we initialize weight randomly at [0, 2/(3*input_size)), bias randomly at [0, 0.5)
+  Notice that when calculate derivative, we don't divide batch size,
+  so the batch size is actually merged into learning rate.
+  We hope the output wouldn't easily fall down to negative value,
+  so we pick (0.5/5/batchSize) as learning rate, which is 0.001
+  """
   learningRate = 0.001
 
   tempTestSize = 3000
 
   for i in range(epoch):
     progress = 0
-    #for j in range(10):
+    # for j in range(10):
     while progress + batchSize < len(train) - tempTestSize:
       batch = [train[i].pixels for i in range(progress, progress + batchSize)]
       batchlabel = [train[i].labelArray for i in range(progress, progress + batchSize)]
       IL.setData(np.array(batch).transpose(), batchSize)
       IL.forward()
       score = np.zeros((output, batchSize))
-      #print OL._output.transpose()
+      # print OL._output.transpose()
       softmax(OL._output, score)
       if verbose:
         print "score:"
@@ -187,7 +217,7 @@ if __name__ == "__main__":
       cross_entrophy = np.multiply(np.array(batchlabel).transpose(), np.log(score))
       if verbose:
         print cross_entrophy.transpose()
-      loss = -np.sum(cross_entrophy, (0,1))/batchSize
+      loss = -np.sum(cross_entrophy, (0, 1)) / batchSize
       print "Loss: %f" % loss
 
       dLdOutput_OL = score - np.array(batchlabel).transpose()
@@ -202,10 +232,10 @@ if __name__ == "__main__":
       right = 0
       for i in range(batchSize):
         if predict[i] == labels[i]:
-          right+=1
+          right += 1
       print "Accuracy: %d/%d" % (right, batchSize)
       progress += batchSize
-      #break
+      # break
 
   batch = [train[i].pixels for i in range(len(train) - tempTestSize, len(train))]
   batchlabel = [train[i].labelArray for i in range(len(train) - tempTestSize, len(train))]
@@ -218,5 +248,5 @@ if __name__ == "__main__":
   right = 0
   for i in range(tempTestSize):
     if predict[i] == labels[i]:
-      right+=1
+      right += 1
   print "\n\nTemporary Test Accuracy: %d/%d" % (right, tempTestSize)
