36a09a9c86ab5ce76eef896864ecd5933d5c5676
Shiliu Wang <shiliu.wang@intel.com>
2018-03-21 22:25:55 +0800

    Implement back prop / param update / backprop check for conv layer

--- layers/conv.py
+++ layers/conv.py
@@ -22,6 +22,16 @@ def _mapping(input, map, output):
     output[coords] = input[tuple(map[coords])]
     flat.next()
 
+def _reverse_mapping(output, map, input):
+  inputDimen = len(input.shape)
+  assert output.shape == map.shape[:-1]
+  assert map.shape[-1] == inputDimen
+  flat = output.flat
+  for i in range(output.size):
+    coords = flat.coords
+    input[tuple(map[coords])] += output[coords]
+    flat.next()
+
 class ConvLayer(Layer):
   def _calcWindows(self):
     self._windowNum = [
@@ -71,6 +81,8 @@ class ConvLayer(Layer):
           self._outputMapping[d * windowSize + i][s][0] = s * windowSize + i
           self._outputMapping[d * windowSize + i][s][1] = d
     self._outputTransform = np.zeros((self._windowNum[0]*self._windowNum[1]*n, self._kernalInfo[DEPTH]))
+    self._dLdOutputTransform = np.zeros(self._outputTransform.shape)
+    self._dLdInputTransform = np.zeros(self._inputTransfom.shape)
 
   def __init__(self, name, depth, kWidth, kHeight, stride, prev, inputWidth, inputHeight, inputChannel):
     self._kernalInfo = [
@@ -90,6 +102,8 @@ class ConvLayer(Layer):
       size=(self._kernalInfo[WIDTH]*self._kernalInfo[HEIGHT]*self._inputInfo[CHANNEL],
             self._kernalInfo[DEPTH]))
     self._bias = np.random.normal(size=(1, self._kernalInfo[DEPTH]))
+    self._dLdKernal = np.zeros(self._kernal.shape)
+    self._dLdb = np.zeros(self._bias.shape)
 
   def setBatchSize(self, n):
     Layer.setBatchSize(self, n)
@@ -102,7 +116,33 @@ class ConvLayer(Layer):
     _mapping(self._outputTransform, self._outputMapping, self._output)
 
   def calcDeri(self):
-    pass
+    _reverse_mapping(self._dLdOutput, self._outputMapping, self._dLdOutputTransform)
+    np.matmul(self._kernal.transpose(), self._dLdOutputTransform, self._dLdInputTransform)
+    _reverse_mapping(self._dLdInputTransform, self._inputMapping, self._dLdInput)
+    np.matmul(self._dLdOutputTransform, self._inputTransfom.transpose(), self._dLdKernal)
+    self._dLdb = np.sum(self._dLdOutputTransform, (0)).reshape((1, self._kernalInfo[DEPTH]))
+
+  def updateParam(self, rate):
+    #Logger.verbose("UPDATE", "update param: %s\n%s" % (self._name, self._dLdw))
+    np.add(self._kernal, self._dLdKernal * -rate, self._kernal)
+    np.add(self._bias, self._dLdb * -rate, self._bias)
+
+  def getDerivative(self, idx):
+    if idx < self._kernal.size:
+      return self._kernal[(idx / self._kernal.shape[1], idx % self._kernal.shape[1])]
+    bidx = idx - self._kernal.size
+    if bidx < self._bias.size:
+      return self._dLdb[(0, bidx)]
+    return None
+
+  def adjustParam(self, idx, delta):
+    if idx < self._kernal.size:
+      self._kernal[(idx / self._kernal.shape[1], idx % self._kernal.shape[1])] += delta
+      return
+    bidx = idx - self._kernal.size
+    if bidx < self._bias.size:
+      self._bias[(0, bidx)] += delta
+      return
 
 if __name__ == "__main__":
   test = ConvLayer("conv", 4, 2, 2, 1, InputLayer(5*5*3), 5, 5, 3)
